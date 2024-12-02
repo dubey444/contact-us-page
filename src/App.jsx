701e147424e486569a8fa7eabbb49df8669bf8dd
dubey444 <mr.dubey44@gmail.com>
2024-12-02 01:01:24 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Contact from './componnets/Contact'
+
+const App = () => {
+  return (
+    <div>
+
+      <Contact/>
+    </div>
+  )
+}
+
+export default App
